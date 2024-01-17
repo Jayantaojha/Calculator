bd22f7eb77e9397f464d6442aaab4088287ced3f
Jayanta Ojha <jayantaojha686@gmail.com>
2024-01-17 22:41:10 +0530

    dark mode is enabled

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const setting = document.getElementById('setting');
 const history = document.getElementById('history');
 const answer = document.getElementById('answer');
@@ -5,30 +6,22 @@ const lastAnswer = document.getElementById('last-answer');
 
 answer.innerText = "";
 
-setting.addEventListener('click', () => {
-    console.log('Setting is clicked');
-})
-
-history.addEventListener('click', () => {
-    console.log('History is clicked');
-})
-
 const buttons = document.querySelectorAll('button');
 const buttonsArr = Array.from(buttons);
 
 buttonsArr.forEach((button) => {
     button.addEventListener('click', () => {
-        if(button.innerText !== "" && button.innerText !== "C"){
+        if (button.innerText !== "" && button.innerText !== "C") {
             answer.innerText += button.innerText
         }
-        else if(button.innerText == "" || button.innerText == "C"){
+        else if (button.innerText == "" || button.innerText == "C") {
             switch (button.id) {
                 case 'clear-all':
                     console.log("Clear all is clicked");
                     answer.innerText = "";
                     lastAnswer.innerText = "";
                     break;
-                
+
                 case 'delete':
                     console.log("Delete is clicked");
                     answer.innerText = answer.innerText.slice(0, -1);
@@ -81,4 +74,71 @@ buttonsArr.forEach((button) => {
             }
         }
     })
+})
+
+
+// div for dark mode
+const div = document.createElement('div');
+div.style.height = "5vh";
+div.style.width = "28vw";
+div.style.backgroundColor = "#f8f8f8";
+div.style.position = "absolute";
+div.style.top = "9vh";
+div.style.left = "5vw";
+div.style.borderRadius = "6px";
+div.style.display = "flex";
+div.style.justifyContent = "center";
+div.style.alignItems = "center";
+div.style.boxShadow = "1px 2px 10px grey";
+div.style.cursor = "pointer";
+div.innerHTML = `<p style="text-align: center; font-size: 12px; color: #525252;">Dark Mode</p>`;
+
+
+
+let clickTime = 0;
+
+setting.addEventListener('click', () => {
+    if (clickTime === 0) {
+        setting.firstChild.classList.remove("fa-gear");
+        setting.firstChild.classList.add("fa-xmark");
+        clickTime = 1;
+
+        body.appendChild(div);
+
+        // for switching color mode
+        let modeCounter = true;
+
+        div.addEventListener('click', () => {
+            // for dark mode
+            if (modeCounter) {
+                div.innerHTML = `<p style="text-align: center; font-size: 12px; color: #525252;">Light Mode</p>`;
+                // body.style.backgroundColor = "#0a2538";
+                body.classList.add('dark');
+                body.classList.add('bg-black');
+                body.classList.remove('bg-white');
+                
+                modeCounter = false;
+            }
+            // for light mode
+            else{
+                div.innerHTML = `<p style="text-align: center; font-size: 12px; color: #525252;">Dark Mode</p>`;
+                // body.style.backgroundColor = "white";
+                body.classList.remove('dark');
+                body.classList.remove('bg-dark');
+                body.classList.add('bg-white');
+
+                modeCounter = true;
+            }
+
+        })
+    }
+
+    else if (clickTime === 1) {
+        console.log("Second time click");
+        body.removeChild(div);
+
+        setting.firstChild.classList.remove("fa-xmark");
+        setting.firstChild.classList.add("fa-gear");
+        clickTime = 0;
+    }
 })
